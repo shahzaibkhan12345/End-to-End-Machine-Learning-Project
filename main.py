0679a4a89495910e97655083aed74b6055c64098
shahzaibkhan12345 <sk4563658@gmail.com>
2025-08-28 23:58:55 +0500

    Data Transformation done

--- main.py
+++ main.py
@@ -21,3 +21,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+STAGE_NAME = "Data Transformation stage"
+from src.mlProject.config.configuration import ConfigurationManager
+from src.mlProject.components.data_transformation import DataTransformation
+try:
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        print(data_transformation_config)
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.train_test_spliting()
+except Exception as e:
+    raise e
